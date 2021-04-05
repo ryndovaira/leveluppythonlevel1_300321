6ad2db74fe0d7688ff4c99aa1d69e21781b7ad01
ryndovaira <ryndovaira@gmail.com>
2021-04-05 17:59:58 +0300

    Update

--- topic_05_data_structure/practice/list_1_count_num.py
+++ topic_05_data_structure/practice/list_1_count_num.py
@@ -5,7 +5,20 @@
 
 Возвращает количество num в списке my_list.
 
+(Порядок проверки именно такой:)
+
 Если вместо списка передано что-то другое, то возвращать строку 'First arg must be list!'.
 Если вместо числа передано что-то другое, то возвращать строку 'Second arg must be int!'.
 Если список пуст, то возвращать строку 'Empty list!'.
 """
+
+
+def count_num(my_list, num):
+    if type(my_list) != list:
+        return 'First arg must be list!'
+    elif type(num) != int:
+        return 'Second arg must be int!'
+    elif len(my_list) == 0:
+        return 'Empty list!'
+
+    return my_list.count(num)
--- topic_05_data_structure/practice/tests/list_1_count_num_test.py
+++ topic_05_data_structure/practice/tests/list_1_count_num_test.py
@@ -1,16 +1,19 @@
 import pytest
 
 from topic_05_data_structure.practice.list_1_count_num import count_num
-
 params = [
     (None, 0, 'First arg must be list!'),
     ([1], None, 'Second arg must be int!'),
-    ([], 1, 'Empty list!\n'),
-    (None, None, 'First arg must be list!\nSecond arg must be int!\n'),
-    ([], None, 'Empty list!\nSecond arg must be int!\n'),
+    ([], 1, 'Empty list!'),
+    (None, None, 'First arg must be list!'),
+    ([], None, 'Second arg must be int!'),
 
-    ([1, 1, 1], 1, 3)
+    ([1, 1, 1], 1, 3),
+    ([1, 1, 1], 3, 0),
+    ([1, 2, 3], 2, 1),
+    ([3, 2, 3], 3, 2)
 ]
+
 
 ids = ["list: %s | num: %s => %s" % (my_list, num, expected) for (my_list, num, expected) in params]
 
