01951512dca871cabc92e10a9a2495263121e19c
ryndovaira <ryndovaira@gmail.com>
2021-04-05 18:01:54 +0300

    Update

--- topic_05_data_structure/practice/list_1_count_num.py
+++ topic_05_data_structure/practice/list_1_count_num.py
@@ -11,14 +11,3 @@
 Если вместо числа передано что-то другое, то возвращать строку 'Second arg must be int!'.
 Если список пуст, то возвращать строку 'Empty list!'.
 """
-
-
-def count_num(my_list, num):
-    if type(my_list) != list:
-        return 'First arg must be list!'
-    elif type(num) != int:
-        return 'Second arg must be int!'
-    elif len(my_list) == 0:
-        return 'Empty list!'
-
-    return my_list.count(num)
