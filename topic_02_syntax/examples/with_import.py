3408c8d04723c706fbb702bafa9d24a80496ef66
ryndovaira <ryndovaira@gmail.com>
2021-04-05 20:44:57 +0300

    Update

---
+++ topic_02_syntax/examples/with_import.py
@@ -0,0 +1,9 @@
+import with_main
+
+with_main.my_code()
+
+print('lolol')
+
+with_main.my_code()
+
+with_main.hi()
---
+++ topic_02_syntax/examples/with_main.py
@@ -0,0 +1,16 @@
+def my_code():
+    print("I'm better!")
+    print("I'm better!")
+    print("I'm better!")
+
+
+def hi():
+    print('hi')
+    print('hi')
+    print('hi')
+    print('hi')
+    print('hi')
+
+
+if __name__ == '__main__':
+    hi()
