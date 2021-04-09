91bfa0d960490e77adaa9a17b53a1887b47400dd
ryndovaira <ryndovaira@gmail.com>
2021-04-09 21:53:55 +0300

    Update

--- topic_02_syntax/practice/string_1_triple_string_len.py
+++ topic_02_syntax/practice/string_1_triple_string_len.py
@@ -6,3 +6,21 @@
 
 Если строка пустая, то вернуть None
 """
+
+
+def triple_string_len(my_str):
+    my_str_len = len(my_str)
+
+    # способ 1:
+    # if my_str_len == 0:
+    #     return None
+    # else:
+    #     return f"{my_str}, {my_str}? {my_str}! len={my_str_len} :)"
+
+    # способ 2:
+    # if my_str_len > 0:
+    #     return f"{my_str}, {my_str}? {my_str}! len={my_str_len} :)"
+
+    # способ 3:
+    if my_str_len:
+        return f"{my_str}, {my_str}? {my_str}! len={my_str_len} :)"
