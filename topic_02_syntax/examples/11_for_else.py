1feb1b3a14c5c48d3c052f004e8104b2eb7e5499
ryndovaira <ryndovaira@gmail.com>
2021-04-09 22:00:43 +0300

    Update

--- topic_02_syntax/examples/11_for_else.py
+++ topic_02_syntax/examples/11_for_else.py
@@ -1,9 +1,12 @@
 for i in 'hello world':
-    if i == 'a':
-        print(f'Буквы {i} в строке есть')
+    # if i == 'a':
+    if i == 'o':
+        print(f'Буква {i} в строке есть')
         break
-else:
+else:   # попадем в else если не было break
     print('Буквы a в строке нет')
 
 # Буквы a в строке нет
+
+print("I'm here!")
 
