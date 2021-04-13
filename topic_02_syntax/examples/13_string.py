f390451ec0ed69f24bb71a74407e30a8cec4f9aa
ryndovaira <ryndovaira@gmail.com>
2021-04-13 19:56:33 +0300

    Update

--- topic_02_syntax/examples/13_string.py
+++ topic_02_syntax/examples/13_string.py
@@ -1,5 +1,40 @@
 print("hello\n\nmy\t\ttab")
 
+empty_str = ""
+
+mul_lines_str = """Привет
+это вторая строка
+это третья строка
+"""
+
+val = 'kdkdj \' djdj " /djdj'
+spec_str = "дщдщ \\ ноно \t jkdjfk \" jdjdj ' fff"
+print(spec_str)
+
+for i in range(1, 9):
+    my_str = "*" * i
+    print(f"{my_str}\t!")
+
 string = "Hello"
 print('string[0:-1]:', string[0:-1])
 print('string[0:0]:', string[0:0])
+print('string[0:99]:', string[0:99])
+print('string[99:100]:', string[99:100])
+
+print(string[4])
+# print(string[99])   # будет ошибка, так как нет символа под номер 100
+
+# ------------------------------
+
+my_long_text = """
+Привет
+это очень сложный
+текст
+имеет много слов
+Конец!
+"""
+
+my_words = my_long_text.split()
+print(my_words)
+print(my_words[3])
+print(my_words[3:26:2])
--- topic_02_syntax/practice/string_3_print_diff_count.py
+++ topic_02_syntax/practice/string_3_print_diff_count.py
@@ -8,3 +8,19 @@
 
 Если строки одинаковой длины, то вывести строку "Equal!".
 """
+
+
+def print_diff_count(s1, s2):
+    diff = len(s1) - len(s2)
+
+    if diff == 0:
+        print("Equal!")
+    elif diff > 0:
+        print(s1 * diff)
+    else:
+        print(s2 * -diff)
+        # print(s2 * abs(diff))
+
+
+if __name__ == '__main__':
+    print_diff_count("123", "123456")
