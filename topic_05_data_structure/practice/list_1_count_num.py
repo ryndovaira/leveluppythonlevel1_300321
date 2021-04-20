8048e4c3983b85c50cc32913751abb7951d378dd
ryndovaira <ryndovaira@gmail.com>
2021-04-20 21:59:11 +0300

    Update

--- topic_05_data_structure/practice/list_1_count_num.py
+++ topic_05_data_structure/practice/list_1_count_num.py
@@ -11,3 +11,30 @@
 Если вместо числа передано что-то другое, то возвращать строку 'Second arg must be int!'.
 Если список пуст, то возвращать строку 'Empty list!'.
 """
+
+
+def count_num(my_list: list, num: int):
+    if type(my_list) != list:
+        return 'First arg must be list!'
+    if type(num) != int:
+        return 'Second arg must be int!'
+    if len(my_list) == 0:
+        return 'Empty list!'
+
+    return my_list.count(num)
+
+
+if __name__ == '__main__':
+    test_list_1 = [1, 3, None, 'lolol']
+    my_num = 99
+    result = count_num(test_list_1, my_num)
+    print(result)
+    print(f'result = {result}')
+
+    # -------
+
+    print('result2 =', count_num([5, 4, 3, 5, '88'], 5))
+
+    # --------------------------
+
+    print(['234', 'uuu', 'uuu', 65, None, 45].count('uuu'))
--- topic_05_data_structure/practice/list_2_magic_parts.py
+++ topic_05_data_structure/practice/list_2_magic_parts.py
@@ -15,3 +15,32 @@
 Если вместо списка передано что-то другое, то возвращать строку 'Must be list!'.
 Если список пуст, то возвращать строку 'Empty list!'.
 """
+
+
+def magic_parts(my_list: list):
+    if type(my_list) != list:
+        return 'Must be list!'
+    elif len(my_list) == 0:
+        return 'Empty list!'
+
+    result_list = []
+
+    # first_el = my_list[0]
+    # так будет ошибка, если в my_list только один элемент при обращении по индексу 1
+    # second_el = my_list[1]
+
+    first_second_el = my_list[:2]
+    last_el = my_list[-1]
+    len_el = len(my_list)
+
+    result_list.extend(first_second_el)     # так как добавляем элементы из списка
+    result_list.append(last_el)             # так как добавляем один элемент
+    result_list.append(len_el)
+
+    return result_list
+
+
+if __name__ == '__main__':
+    magic_parts([1])
+    magic_parts([1, 5])
+    magic_parts([1, 5, 3])
