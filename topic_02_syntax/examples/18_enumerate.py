9ae83cef5f2b4eb52492179b4b2d70e68a16a059
ryndovaira <ryndovaira@gmail.com>
2021-04-20 18:25:19 +0300

    Update

--- topic_02_syntax/examples/18_enumerate.py
+++ topic_02_syntax/examples/18_enumerate.py
@@ -8,34 +8,3 @@ for index, char in enumerate("qwertyu"):
         print("Even!")
 
 print('-' * 50)
-
-# ------------------------------------
-my_list = [1, 'tytyt', 2, 3]
-
-for index, element in enumerate(my_list):
-    print(f"index: {index}\t"
-          f"element: {element}")
-
-print('-' * 50)
-
-my_dict = {1: 'a', 2: 'b', 44: 'zz'}
-for index, (key, value) in enumerate(my_dict.items()):
-    print(f"index: {index}\t"
-          f"key: {key}\t"
-          f"value: {value}")
-
-print('-' * 50)
-# ИЛИ
-
-for index, key_value in enumerate(my_dict.items()):
-    print(f"index: {index}\t"
-          f"key: {key_value[0]}\t"
-          f"value: {key_value[1]}\t"
-          f"key_value: {key_value}")
-
-print('-' * 50)
-
-my_set = {1, 2, 3, 2, 1}
-for index, element in enumerate(my_set):
-    print(f"index: {index}\t"
-          f"element: {element}")
